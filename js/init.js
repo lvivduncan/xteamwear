70f1468820a51c231612a5a4456b5d595794c1b0
lvivduncan <lvivduncan@gmail.com>
2020-12-19 21:23:25 +0200

    category.html

--- js/init.js
+++ js/init.js
@@ -1,31 +1,3 @@
-// slider
-(function () {
-    var $example = $('#slider');
-    var frame = $example.find('.container')[0];
-    var offset = $example.offset();
-    var motio = new Motio(frame, {
-        fps: 30,
-        bgWidth: 1024,
-        bgHeight: 1024
-    });
-
-    $example.on('mouseenter mouseleave', function (event) {
-        if (event.type === 'mouseenter') {
-            motio.play();
-        } else {
-            motio.pause();
-        }
-    });
-
-    $(window).on('resize', function () {
-        offset = $example.offset();
-    });
-
-    $example.on('mousemove', function (event) {
-        motio.set('speedX', event.pageX - offset.left - motio.width / 2);
-        motio.set('speedY', event.pageY - offset.top - motio.height / 2);
-    });
-}());
 
 $(window).scroll(function(){
     if ($(this).scrollTop() > 50) {
